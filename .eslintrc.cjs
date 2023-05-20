46fb7b73eb22b73a88b3a9afc57d33f9d93c5f7f
shenchao <nasus0315@163.com>
2023-05-20 21:56:05 +0800

    规则配置 - 1

---
+++ .eslintrc.cjs
@@ -0,0 +1,43 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true,
+    },
+    extends: [
+        "plugin:vue/vue3-recommended", 
+        "plugin:prettier/recommended"
+    ],
+    "overrides": [
+    ],
+    // 指定如何解析语法
+	parser: "vue-eslint-parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+        "no-console": 2,
+        "vue/html-indent": ["error", 4],
+        "vue/html-self-closing": ["error", {
+            "html": {
+                "void": "always",
+                "normal": "never",
+                "component": "always"
+            },
+            "svg": "always",  // svg标签自闭合
+            "math": "always"  // MathML标签自闭合
+        }],
+        "vue/max-attributes-per-line": ["error", {
+            "singleline": {
+                "max": 1
+            },      
+            "multiline": {
+                "max": 1
+            }
+        }]
+    }
+}
